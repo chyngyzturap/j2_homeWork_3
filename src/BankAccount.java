063cd53d7891d3d719c5f77931758ecf764b0622
Chyngyz <chyngyzturapov@gmail.com>
2020-10-29 14:34:32 +0600

    up

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,18 +5,19 @@ public class BankAccount {
         return amount;
     }
 
-    public void deposit (double sum){
-        amount = amount + sum;
+    public void deposit(double sum) {
+        amount += sum;
         System.out.println("На вашем балансе: " + amount);
     }
-    public void WithDraw (int sum) throws LimitException {
-        amount = amount - sum;
+
+    public void WithDraw(int sum) throws LimitException {
+        if (amount < sum) {
+            throw new LimitException("Не хватает денег на балансе", getAmount());
+        }
+        amount -= sum;
         System.out.println("C вашего баланса было вычитано: -" + sum);
         System.out.println("На вашем балансе осталось: " + amount);
 
-       if (amount<sum){
-           throw new LimitException("Не хватает денег на балансе", getAmount());
-       }
 
     }
 }
